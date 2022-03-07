fbcf325b0722687236bb8b78dbf93ac393b8c02d
Dmitry Novik <dmitry.novik@gmail.com>
2022-03-07 14:49:31 +1100

    Add project files.

---
+++ XUnitLib.Test/Test.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using FluentAssertions;
+using XUnitLib;
+
+public class Test
+{
+    [Fact] public void Foo() 
+    {
+        var input = new[] { 1, 2, -1, -1, 3 };
+        LongestSubSum.Compute(input).Should().Be(4);
+    }
+
+    [Fact] public void Bar()
+    {
+        var input = new[] { 7, 0, -1, 0, -1 };
+        LongestSubSum.Compute(input).Should().Be(7);
+    }
+
+    [Fact] public void Baz()
+    {
+        var input = new[] { -1, 1, 2, -1, -1, 3 };
+        LongestSubSum.Compute(input).Should().Be(4);
+    }
+
+    [Fact] public void Foobar()
+    {
+        var input = new[] { 7, -1, -1, -1, 4 };
+        LongestSubSum.Compute(input).Should().Be(8);
+    }
+
+}
---
+++ XUnitLib/LongestSubSum.cs
@@ -0,0 +1,22 @@
+﻿namespace XUnitLib
+{
+    public static class LongestSubSum
+    {
+        public static int Compute(int[] a)
+        {
+            int current = 0, longest = 0;
+
+            for (var i = 0; i < a.Length; ++i)
+            {
+                current += a[i];
+
+                if (current < 0)
+                    current = 0;
+                else if (current > longest)
+                    longest = current;
+            }
+
+            return longest;
+        }
+    }
+}
